477bc6416f510766c07d5fbdddea783dd66aaf65
ianh8899 <ihitchman83@gmail.com>
2025-06-29 12:34:13 +0100

    release build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: true,
-    allowedHosts: true,
-  },
-  server: {
-    allowedHosts: [
-      'localhost',
-      'client.ianhitchman.co.uk'
-    ],
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    preview: {
+      host: true,
+      allowedHosts: true,
+    },
+    server: {
+      allowedHosts: [
+        "localhost",
+        new URL(env.VITE_PROD_URL || "http://localhost").hostname,
+      ],
+    },
+  };
 });
