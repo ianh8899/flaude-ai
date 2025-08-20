b74ade06d2a6e5e61bbb94c7dfae0c6e9579d93f
ianh8899 <ihitchman83@gmail.com>
2025-08-20 16:44:56 +0100

    add waiting message - update build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,6 @@ import { Chat } from "./components/Chat";
 
 function App() {
   const { data: session, isPending } = useSession();
-  console.log("Session Data:", session);
 
   if (isPending) {
     return <div>Loading...</div>;
